9ba8818bd2f024f6a6d81aa3fc62f4d6ca6134fd
Franco Bobadilla <1709673@ucc.edu.ar>
2020-09-17 21:51:45 -0300

    Added docstring to decay and neighbourhood functions

--- neural_map/_decay_functions.py
+++ neural_map/_decay_functions.py
@@ -1,22 +1,185 @@
+"""
+Utility functions for computing the decay of some variable over time (steps)
+
+Functions
+--------------
+.. autosummary::
+   :toctree: generated/
+   linear
+   exponential
+   rational
+   no_decay
+
+"""
 from numba import jit, float64
 
 
 @jit(float64(float64, float64, float64, float64), nopython=True, fastmath=True)
 def linear(init, final, total_steps, step):
+    """
+    Compute the linear interpolation between two given values
+    and return the required intermediate value
+
+    Parameters
+    ----------
+    init: float
+        First value
+    final: float
+        Last value
+    total_steps: float
+        Number of intermediate points
+    step: float
+        Intermediate point to compute
+
+    Raises
+    ------
+    ZeroDivisionError
+        Raised when `total_steps` is equal to 1
+        Instead use the `no_decay` function.
+
+    Returns
+    -------
+    interpolation: float
+        Value of the `step` point in the interpolation
+
+    Example
+    -------
+    >>> from neural_map import linear
+    >>> linear(1.0, 0.1, 10, 5)
+    0.5
+
+    """
     return init + step * (final - init) / (total_steps - 1)
 
 
 @jit(float64(float64, float64, float64, float64), nopython=True, fastmath=True)
 def exponential(init, final, total_steps, step):
+    """
+    Compute the exponential interpolation between two given values
+    and return the required intermediate value
+
+    Parameters
+    ----------
+    init: float
+        First value
+    final: float
+        Last value
+    total_steps: float
+        Number of intermediate points
+    step: float
+        Intermediate point to compute
+
+    Raises
+    ------
+    ZeroDivisionError
+        Raised when `total_steps` is equal to 1 or `init` is equal to 0
+        Instead use the `no_decay` function.
+
+    Returns
+    -------
+    interpolation: float
+        Value of the `step` point in the interpolation
+
+    Notes
+    -----
+    The exponential interpolation is given by an exponential function
+    that matches both ``(0, init)`` and ``(total_steps - 1, final)`` points
+
+    .. math::
+
+    f(x) = M * e ^ (t * x)
+    f(0) = init
+    f(total_steps - 1) = final
+
+    Example
+    -------
+    >>> from neural_map import exponential
+    >>> exponential(1.0, 0.1, 10, 5)
+    0.2782559402207124
+
+    """
     return init * (final / init) ** (step / (total_steps - 1))
 
 
 @jit(float64(float64, float64, float64, float64), nopython=True, fastmath=True)
 def rational(init, final, total_steps, step):
+    """
+    Compute the rational interpolation between two given values
+    and return the required intermediate value
+
+    Parameters
+    ----------
+    init: float
+        First value
+    final: float
+        Last value
+    total_steps: float
+        Number of intermediate points
+    step: float
+        Intermediate point to compute
+
+    Raises
+    ------
+    ZeroDivisionError
+        Raised when `steps + (total_steps - 1) / ((init / final) - 1)` is equal to 0
+        or when `final` is equal to 0
+        Instead use the `no_decay` function.
+
+    Returns
+    -------
+    interpolation: float
+        Value of the `step` point in the interpolation
+
+    Notes
+    -----
+    The rational interpolation is given by an simple rational function
+    that matches both ``(0, init)`` and ``(total_steps - 1, final)`` points
+
+    .. math::
+
+    f(x) = a / (b + x)
+    f(0) = init
+    f(total_steps - 1) = final
+
+    Example
+    -------
+    >>> from neural_map import rational
+    >>> rational(1.0, 0.1, 10, 5)
+    0.16666666666666666
+
+    """
     value = (total_steps - 1) / ((init / final) - 1)
     return init * value / (value + step)
 
 
 @jit(float64(float64, float64, float64, float64), nopython=True, fastmath=True)
 def no_decay(init, final, total_steps, step):
+    """
+    Return the init value, regardless of all others
+
+    Parameters
+    ----------
+    init: float
+        First value
+    final: float
+        Last value.
+        This argument is ignored
+    total_steps: float
+        Number of intermediate points.
+        This argument is ignored
+    step: float
+        Intermediate point to compute.
+        This argument is ignored
+
+    Returns
+    -------
+    init: float
+        Value entered in the `init` parameter
+
+    Notes
+    -----
+    Useful when it is needed a function that has the same firm as all others
+    decay functions, but it is no needed to compute any decay
+
+    """
     return init
--- neural_map/_neighbourhood_functions.py
+++ neural_map/_neighbourhood_functions.py
@@ -1,9 +1,61 @@
+"""
+Utility functions for computing the neighbourhood of the best matching unit
+
+Functions
+--------------
+.. autosummary::
+   :toctree: generated/
+   bubble
+   conical
+   gaussian
+   gaussian_cut
+   mexican_hat
+   no_neighbourhood
+
+"""
 from numba import jit, float64
 from numpy import empty, exp
 
 
 @jit(float64[:, :](float64[:, :, :], float64[:], float64, float64), nopython=True, fastmath=True)
 def bubble(positions, bmu, radius, learning_rate):
+    """
+    Compute a circular area of radius `radius` centered at `bmu` over the
+    nodes at positions `positions` and then multiply all by `learning_rate`
+
+    Parameters
+    ----------
+    positions: ndarray
+        Cartesian coordinates of the nodes in the 2D virtual space.
+        Dimensions should be (x, y, 2)
+    bmu: ndarray
+        Cartesian coordinate of the best matching unit in the 2D virtual space.
+        Dimensions should be (2)
+    radius: float
+        Radius of neighbourhood
+    learning_rate: float
+        Multiplication factor
+
+    Raises
+    ------
+    ZeroDivisionError
+        Raised when `radius` is equal to 0
+        Instead use the `no_neighbourhood` function.
+
+    Returns
+    -------
+    update_map: ndarray
+        Array with each node update.
+        Dimensions are (x, y)
+
+    Example
+    -------
+    >>> from numpy import array
+    >>> from neural_map import bubble
+    >>> bubble(array([[[0., 0.], [0., 1.]], [[1., 0.], [1., 1.]]]), array([1., 1.]), 1.4, 0.5)
+    array([[0., 0.5], [0.5, 0.5]])
+
+    """
     res = empty((positions.shape[0], positions.shape[1]), dtype=positions.dtype)
     for i in range(positions.shape[0]):
         for j in range(positions.shape[1]):
@@ -17,6 +69,43 @@ def bubble(positions, bmu, radius, learning_rate):
 
 @jit(float64[:, :](float64[:, :, :], float64[:], float64, float64), nopython=True, fastmath=True)
 def conical(positions, bmu, radius, learning_rate):
+    """
+    Compute a conical area of radius `radius` centered at `bmu` over the
+    nodes at positions `positions` and then multiply all by `learning_rate`
+
+    Parameters
+    ----------
+    positions: ndarray
+        Cartesian coordinates of the nodes in the 2D virtual space.
+        Dimensions should be (x, y, 2)
+    bmu: ndarray
+        Cartesian coordinate of the best matching unit in the 2D virtual space.
+        Dimensions should be (2)
+    radius: float
+        Radius of neighbourhood
+    learning_rate: float
+        Multiplication factor
+
+    Raises
+    ------
+    ZeroDivisionError
+        Raised when `radius` is equal to 0
+        Instead use the `no_neighbourhood` function.
+
+    Returns
+    -------
+    update_map: ndarray
+        Array with each node update.
+        Dimensions are (x, y)
+
+    Example
+    -------
+    >>> from numpy import array
+    >>> from neural_map import bubble
+    >>> bubble(array([[[0., 0.], [0., 1.]], [[1., 0.], [1., 1.]]]), array([1., 1.]), 1.4, 0.5)
+    array([[0., 0.14285714], [0.14285714, 0.5]])
+
+    """
     res = empty((positions.shape[0], positions.shape[1]), dtype=positions.dtype)
     for i in range(positions.shape[0]):
         for j in range(positions.shape[1]):
@@ -27,6 +116,43 @@ def conical(positions, bmu, radius, learning_rate):
 
 @jit(float64[:, :](float64[:, :, :], float64[:], float64, float64), nopython=True, fastmath=True)
 def gaussian(positions, bmu, radius, learning_rate):
+    """
+    Compute a gaussian area of standard deviation `radius` centered at `bmu` over the
+    nodes at positions `positions` and then multiply all by `learning_rate`
+
+    Parameters
+    ----------
+    positions: ndarray
+        Cartesian coordinates of the nodes in the 2D virtual space.
+        Dimensions should be (x, y, 2)
+    bmu: ndarray
+        Cartesian coordinate of the best matching unit in the 2D virtual space.
+        Dimensions should be (2)
+    radius: float
+        Standard deviation
+    learning_rate: float
+        Multiplication factor
+
+    Raises
+    ------
+    ZeroDivisionError
+        Raised when `radius` is equal to 0
+        Instead use the `no_neighbourhood` function.
+
+    Returns
+    -------
+    update_map: ndarray
+        Array with each node update.
+        Dimensions are (x, y)
+
+    Example
+    -------
+    >>> from numpy import array
+    >>> from neural_map import gaussian
+    >>> gaussian(array([[[0., 0.], [0., 1.]], [[1., 0.], [1., 1.]]]), array([1., 1.]), 1.4, 0.5)
+    array([[0.18022389, 0.30018652], [0.30018652, 0.5]])
+
+    """
     res = empty((positions.shape[0], positions.shape[1]), dtype=positions.dtype)
     for i in range(positions.shape[0]):
         for j in range(positions.shape[1]):
@@ -37,6 +163,44 @@ def gaussian(positions, bmu, radius, learning_rate):
 
 @jit(float64[:, :](float64[:, :, :], float64[:], float64, float64), nopython=True, fastmath=True)
 def gaussian_cut(positions, bmu, radius, learning_rate):
+    """
+    Compute a gaussian area of standard deviation `radius` centered at `bmu` over the
+    nodes at positions `positions`, then multiply all by `learning_rate`,
+    and set to 0 all nodes further than `radius` from `bmu`
+
+    Parameters
+    ----------
+    positions: ndarray
+        Cartesian coordinates of the nodes in the 2D virtual space.
+        Dimensions should be (x, y, 2)
+    bmu: ndarray
+        Cartesian coordinate of the best matching unit in the 2D virtual space.
+        Dimensions should be (2)
+    radius: float
+        Radius of neighbourhood and standard deviation
+    learning_rate: float
+        Multiplication factor
+
+    Raises
+    ------
+    ZeroDivisionError
+        Raised when `radius` is equal to 0
+        Instead use the `no_neighbourhood` function.
+
+    Returns
+    -------
+    update_map: ndarray
+        Array with each node update.
+        Dimensions are (x, y)
+
+    Example
+    -------
+    >>> from numpy import array
+    >>> from neural_map import gaussian_cut
+    >>> gaussian_cut(array([[[0., 0.], [0., 1.]], [[1., 0.], [1., 1.]]]), array([1., 1.]), 1.4, 0.5)
+    array([[0., 0.30018652], [0.30018652, 0.5]])
+
+    """
     res = empty((positions.shape[0], positions.shape[1]), dtype=positions.dtype)
     for i in range(positions.shape[0]):
         for j in range(positions.shape[1]):
@@ -51,6 +215,43 @@ def gaussian_cut(positions, bmu, radius, learning_rate):
 
 @jit(float64[:, :](float64[:, :, :], float64[:], float64, float64), nopython=True, fastmath=True)
 def mexican_hat(positions, bmu, radius, learning_rate):
+    """
+    Compute a mexican hat wavelet area of standard deviation `radius` centered at `bmu`
+    over the nodes at positions `positions`, then multiply all by `learning_rate`
+
+    Parameters
+    ----------
+    positions: ndarray
+        Cartesian coordinates of the nodes in the 2D virtual space.
+        Dimensions should be (x, y, 2)
+    bmu: ndarray
+        Cartesian coordinate of the best matching unit in the 2D virtual space.
+        Dimensions should be (2)
+    radius: float
+        Radius of neighbourhood and standard deviation
+    learning_rate: float
+        Multiplication factor
+
+    Raises
+    ------
+    ZeroDivisionError
+        Raised when `radius` is equal to 0
+        Instead use the `no_neighbourhood` function.
+
+    Returns
+    -------
+    update_map: ndarray
+        Array with each node update.
+        Dimensions are (x, y)
+
+    Example
+    -------
+    >>> from numpy import array
+    >>> from neural_map import mexican_hat
+    >>> mexican_hat(array([[[0., 0.], [0., 1.]], [[1., 0.], [1., 1.]]]), array([1., 1.]), 1.4, 0.5)
+    array([[-0.00367804, 0.14703013], [0.14703013, 0.5]])
+
+    """
     res = empty((positions.shape[0], positions.shape[1]), dtype=positions.dtype)
     for i in range(positions.shape[0]):
         for j in range(positions.shape[1]):
@@ -62,6 +263,36 @@ def mexican_hat(positions, bmu, radius, learning_rate):
 
 @jit(float64[:, :](float64[:, :, :], float64[:], float64, float64), nopython=True, fastmath=True)
 def no_neighbourhood(positions, bmu, radius, learning_rate):
+    """
+    Set the `learning_rate` value to the node with the `bmu` position,
+    and set to 0 all others
+
+    Parameters
+    ----------
+    positions: ndarray
+        Cartesian coordinates of the nodes in the 2D virtual space.
+        Dimensions should be (x, y, 2)
+    bmu: ndarray
+        Cartesian coordinate of the best matching unit in the 2D virtual space.
+        Dimensions should be (2)
+    radius: float
+        Radius of neighbourhood and standard deviation.
+        This argument is ignored
+    learning_rate: float
+        Multiplication factor
+
+    Returns
+    -------
+    update_map: ndarray
+        Array with each node update.
+        Dimensions are (x, y)
+
+    Notes
+    -----
+    Useful when it is needed a function that has the same firm as all others
+    neighbourhood functions, but it is no needed to compute any real neighbourhood
+
+    """
     res = empty((positions.shape[0], positions.shape[1]), dtype=positions.dtype)
     for i in range(positions.shape[0]):
         for j in range(positions.shape[1]):
